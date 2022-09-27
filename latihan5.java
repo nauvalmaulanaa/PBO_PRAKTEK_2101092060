7bcce33a3da4ba4f51b77df20d57b632f0ce131b
Nauval Maulana <nauvaljr2002@gmail.com>
2022-09-27 21:05:05 +0700

    Create latihan5.java

---
+++ latihan5.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nauval230922;
+
+/**
+ *
+ * @author Nauval
+ */
+public class latihan5 {
+    public static void main(String[] args) {
+        
+        int i = 0;
+        int j = 10;
+        boolean test = false;
+        
+        //demontrasi ||
+            test = (i < 10) || (j++ > 9);
+            System.out.println(i);
+            System.out.println(j);
+            System.out.println(test);
+            
+        //demontrasi |
+            test = (i < 10) | (j++ > 9);
+            System.out.println(i);
+            System.out.println(j);
+            System.out.println(test);
+    }
+}
