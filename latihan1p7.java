c347d4604e9ae967719319a4296573ed71508ec7
Nauval Maulana <nauvaljr2002@gmail.com>
2022-11-03 22:47:42 +0700

    Create latihan1p7.java

---
+++ latihan1p7.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nauval281022;
+
+/**
+ *
+ * @author Nauval
+ */
+public class latihan1p7 {
+    public static void main(String[] args) {
+        int i = 0;
+        String days[] = {"Senin","Selasa","Rabu","Kamis","Jum'at","Sabtu","Minggu"};
+        
+        while (i < 7){
+            System.out.println(days[i]);
+            i++;
+        }
+    }
+}
