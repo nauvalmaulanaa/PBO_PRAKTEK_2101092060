e26774a499a07819a77347f02fb7a91bca4d7757
Nauval Maulana <nauvaljr2002@gmail.com>
2023-01-08 22:18:36 +0700

    Create HelloWorld.java

---
+++ nauval160922/HelloWorld.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nauval160922;
+
+/**
+ *
+ * @author Nauval
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Biodata Diri");
+        
+        String nama, prodi, jurusan, alamat;
+        int nobp;
+        
+        nama = "Nauval";
+        nobp = 2101092060;
+        prodi = "Manajemen Informatika";
+        jurusan = "Teknologi Informasi";
+        alamat = "Jl Seberang Padang";
+   
+        System.out.println("Nama    :" +nama);
+        System.out.println("Nobp    :" +nobp);
+        System.out.println("Prodi   :" +prodi);
+        System.out.println("Jurusan :" +jurusan);
+        System.out.println("Alamat  :" +alamat+"\n");
+        
+        
+        System.out.println("Nama\t:Nauval\n"+"Nobp\t:2101092060\n"+"Prodi\t:MI\n"+
+                "Jurusan\t:TI\n"+"Alamat\t:Jl.Seberang Padang\n");
+        
+        
+        
+    }
+}
