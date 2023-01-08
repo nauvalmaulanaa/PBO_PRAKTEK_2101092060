4122fd31fb0ced12eb0c43b250a20993fd87f8e4
Nauval Maulana <nauvaljr2002@gmail.com>
2023-01-08 22:18:46 +0700

    Create latihan3071022.java

---
+++ nauval071022/latihan3071022.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nauval071022;
+
+/**
+ *
+ * @author Nauval
+ */
+public class latihan3 {
+    public static void main(String[] args) {
+        int gol, gaji_;
+    }   
+}
