b08b4d6effa0a43d4bc6dcc5cf3db37d70af488e
Nauval Maulana <nauvaljr2002@gmail.com>
2023-01-08 22:17:13 +0700

    Create latihan6.java

---
+++ nauval300922/latihan6.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nauval300922;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Nauval
+ */
+public class latihan6 {
+ public static void main( String[] args ){
+ String name1 ="";
+ String name2 ="";
+ String name3 ="";
+ 
+ name1 = JOptionPane.showInputDialog("Enter word 1");
+ name2 = JOptionPane.showInputDialog("Enter word 2");
+ name3 = JOptionPane.showInputDialog("Enter word 3");
+
+ String msg = name1 + "\t" +  name2 + "\t" + name3;
+
+ JOptionPane.showMessageDialog(null, msg);
+
+ } 
+}
