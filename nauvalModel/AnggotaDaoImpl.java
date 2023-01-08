a2366c45643d97c0bece9caab5183b44e1daae17
Nauval Maulana <nauvaljr2002@gmail.com>
2023-01-08 22:17:01 +0700

    Create AnggotaDaoImpl.java

---
+++ nauvalModel/AnggotaDaoImpl.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nauvalModel;
+
+import java.util.List;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Nauval
+ */
+public class AnggotaDaoImpl implements AnggotaDao {
+
+    private List<Anggota> data = new ArrayList<Anggota>();
+    
+    @Override
+    public Anggota save(Anggota anggota) {
+        data.add(anggota);
+        return anggota;
+    }
+
+    @Override
+    public Anggota update(int index, Anggota anggota) {
+        data.set(index, anggota);
+        return anggota;
+    }
+
+    @Override
+    public void delete(int index) {
+        data.remove(index);
+    }
+
+    @Override
+    public Anggota getAnggota(int index) {
+        return data.get(index);
+    }
+
+    @Override
+    public List<Anggota> getAllAnggota() {
+        return data;
+    }
+}
